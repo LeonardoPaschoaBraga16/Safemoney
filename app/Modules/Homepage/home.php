835ed7e8d28cfe24231dbee67ec78883366f6035
Leonardo Paschoa <78983450+LeonardoPaschoaBraga16@users.noreply.github.com>
2026-08-17 06:19:48 -0300

    Starting Project

---
+++ app/Modules/Homepage/home.php
@@ -0,0 +1,15 @@
+<?php require __DIR__ . '../../components/index.php' ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem-Vindo!</title>
+</head>
+<body>
+    <h1>Seja bem vindo ao Safemoney!</h1>
+
+    <h3>Aprenda a utilizar seu dinheiro de forma segura</h3>
+</body>
+</html>
---
+++ app/Modules/components/index.php
@@ -0,0 +1,9 @@
+
+<header>
+        <nav>
+            <a href="../Homepage/home.php">Inicio</a>
+            <a href="../Homepage/sobre.php">Sobre nós</a>
+            <a href="../Homepage/objetivo.php">Nosso objetivo</a>
+            <a href="../Homepage/materiais.php">Iniciar</a>
+        </nav>
+</header>
